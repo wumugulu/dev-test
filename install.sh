995e3c229fc0a10cb36c7b203666b84f6f2423df
wumugulu <lonebat@163.com>
2022-01-18 14:30:22 +0800

    add readme.md

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 
 
 # 利用maven镜像下载代码,构建应用
+echo
+echo ----------------1. 开始下载代码,构建应用......
+echo
+
 DEMO_FILE_DIR=/home/apps
 sudo rm -f $DEMO_FILE_DIR/*
 docker rm -f tmp-maven
-docker run -it --name tmp-maven -v $DEMO_FILE_DIR:/home/apps-docker \
-      maven:3.8.4-jdk-8 /bin/bash -c \
+docker run -it --name tmp-maven -v $DEMO_FILE_DIR:/home/apps-docker maven:3.8.4-jdk-8
+      /bin/bash -c \
       "cd /home/apps-docker && \
       rm -rf /home/apps-docker/* && \
       git clone git://github.com/wumugulu/dev-test.git && \
@@ -14,13 +18,22 @@ docker run -it --name tmp-maven -v $DEMO_FILE_DIR:/home/apps-docker \
       exit"
 
 # 开始部署 ...
+echo
+echo ----------------2. 开始部署......
+echo
 
 # 1. 拉取,并运行nacos
+echo
+echo ------------------2.1 开始运行nacos容器......
+echo
 # docker pull nacos/nacos-server:2.0.3-slim
 docker rm -f test-nacos
 docker run -d --name test-nacos -e MODE=standalone -e JVM_XMS=128m -e JVM_XMX=128m -p 8848:8848 nacos/nacos-server:2.0.3-slim
 
 # 2. 部署myslq以及app
+echo
+echo ------------------2.2 开始构建和运行mysql容器......
+echo
 DEMO_PROJECT_DIR=$DEMO_FILE_DIR/dev-test
 
 # 构建,并运行自己的mysql
@@ -31,7 +44,10 @@ docker rm -f test-mysql
 docker run -d --name test-mysql -e MYSQL_ROOT_PASSWORD=123456 -p 3306:3306 -p 33060:33060 wumugulu/mysql:0.2
 
 # 构建,并运行自己的app容器
-# 1.准备jar包及启动脚本
+echo
+echo ------------------2.3 开始构建和运行apps容器......
+echo
+# 3.准备jar包及启动脚本
 DEMO_APPS_DIR=$DEMO_PROJECT_DIR/file/shell/apps
 sudo rm -rf $DEMO_APPS_DIR/jar
 sudo mkdir -p $DEMO_APPS_DIR/jar
@@ -53,3 +69,9 @@ docker run -d --name test-apps -p 8000:8000 --link test-nacos:nacos-host --link 
 # docker run -it --name test-apps -p 8000:8000 --link test-nacos:nacos-host --link test-mysql:mysql-host wumugulu/myapps:0.3 /bin/bash
 docker ps
 docker logs -f test-apps
+
+echo
+echo ----------------3. 部署完成!!!
+echo
+
+echo please open your browser and access:  http://[YOUR_DOCKER_HOST_IP]:8000/
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
-## 简介
+## 一、简介
  实现新用户注册(发送邮件通知)以及修改,锁定,删除的demo功能演示
 
-## 代码相关
+## 二、代码介绍
   * springcloud, springboot, nacos, mysql, mybatis, mybatis-plus
   * 应用共分为4个微服务模块(和若干公用模块),分别为
     - gateway -- 网关模块
@@ -122,13 +122,44 @@ dev-test.
 ```
     
 
-## 安装运行说明(docker环境部署, 代码保存路径/home/apps)
+## 三、安装部署流程
+(docker环境部署, 代码、jar包保存路径/home/apps)
+
 #### 1. (获取并)运行maven-docker镜像进行代码拉取和打包jar
+* 启动maven容器
+* 进入容器(映射宿主机的文件夹/home/apps)
+* git拉取代码
+* mvn命令打包项目
+* 退出
     
 #### 2. (获取并)运行nacos-docker镜像作为注册中心
+* 拉取并启动nacos容器
+* 命名为test-nacos(后面会用到)
+* 开放端口8848
+
 #### 3. (获取和)构建自己的mysql-docker镜像并运行
+* 构建自己的mysql镜像(主要是导入数据库初始化脚本)
+* 启动自己的数据库容器,命名为test-mysql(后面会用到)
+* 开放端口3306和33060
+
 #### 4. (基于java8)构建自己的apps-docker镜像并运行
+* 准备所需的jar包(4个:gateway,user,mail,webui)和启动脚本(startup.sh)
+* 基于java8构建自己的apps镜像
+* 启动apps容器
+* 开放访问端口8000
+
 #### 5. 浏览器访问前端页面测试功能
+* http://[docker_host_ip]:8000/
 
+## 四、安装说明
+```shell
+# 获取安装脚本
+wget -P /tmp https://github.com/wumugulu/dev-test/blob/main/install.sh
 
+# 添加执行权限 
+sudo chmod +x /tmp/install.sh
+
+# 执行安装脚本
+sh /tmp/install.sh
+```
 
