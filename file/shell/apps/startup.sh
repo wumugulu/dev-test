2fa5502155540cbb99d9f5cf21f44c9f40e2d4bc
wumugulu <lonebat@163.com>
2022-01-19 16:08:58 +0800

    log dir

--- file/shell/apps/startup.sh
+++ file/shell/apps/startup.sh
@@ -8,16 +8,16 @@
 #echo "0: `date`" >> /tmp/apps-docker/debug.log
 
 sleep 3s
-nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m -Dtest.apps.path=/home/apps /home/apps/app-gateway/TestGateway-1.0.jar &
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m /home/apps/app-gateway/TestGateway-1.0.jar &
 
 sleep 3s
-nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m -Dtest.apps.path=/home/apps  /home/apps/app-service-mail/TestMail-1.0.jar &
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m /home/apps/app-service-mail/TestMail-1.0.jar &
 
 sleep 3s
-nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m -Dtest.apps.path=/home/apps /home/apps/app-service-user/TestUser-1.0.jar &
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m /home/apps/app-service-user/TestUser-1.0.jar &
 
 sleep 3s
-nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m -Dtest.apps.path=/home/apps /home/apps/app-web/TestWeb-1.0.jar &
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m /home/apps/app-web/TestWeb-1.0.jar &
 
 while [[ true ]]; do
     sleep 3
