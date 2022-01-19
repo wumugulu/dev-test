42c821a2c4bc98d17f80f16139d28b565d82d2fa
wumugulu <lonebat@163.com>
2022-01-19 13:58:14 +0800

    更新apps image

--- file/shell/apps/startup.sh
+++ file/shell/apps/startup.sh
@@ -1,18 +1,24 @@
+#!/bin/bash
 
 # nohup java -jar /home/apps/app-gateway/TestGateway-1.0.jar > /dev/null 2>&1 &
 # nohup java -jar /home/apps/app-service-mail/TestMail-1.0.jar > /dev/null 2>&1 &
 # ohup java -jar /home/apps/app-service-user/TestUser-1.0.jar > /dev/null 2>&1 &
 # nohup java -jar /home/apps/app-web/TestWeb-1.0.jar > /dev/null 2>&1 &
 
-java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-gateway/TestGateway-1.0.jar > /dev/null &
+#echo "0: `date`" >> /tmp/apps-docker/debug.log
+
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-gateway/TestGateway-1.0.jar &
 sleep 3s
-java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-service-mail/TestMail-1.0.jar > /dev/null &
+
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-service-mail/TestMail-1.0.jar &
 sleep 3s
-java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-service-user/TestUser-1.0.jar > /dev/null &
+
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-service-user/TestUser-1.0.jar &
 sleep 3s
-java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-web/TestWeb-1.0.jar > /dev/null &
+
+nohup java -jar -Xmx128m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m  /home/apps/app-web/TestWeb-1.0.jar &
 sleep 3s
 
 while [[ true ]]; do
-  sleep 5s
+    sleep 3
 done
--- install.sh
+++ install.sh
@@ -29,27 +29,30 @@ echo
 docker rm -f test-nacos
 docker run -d --name test-nacos -e MODE=standalone -e JVM_XMS=128m -e JVM_XMX=128m -p 8848:8848 nacos/nacos-server:2.0.3-slim
 
-# 2. 部署myslq以及app
+# 2. 部署myslq
 echo
 echo ------------------2.2 build mysql image and run my mysql container......
 echo
 DEMO_PROJECT_DIR=/home/apps/dev-test
+DEMO_MYSQL_DIR=$DEMO_PROJECT_DIR/file/shell/mysql
 
 # 构建,并运行自己的mysql
-DEMO_MYSQL_DIR=$DEMO_PROJECT_DIR/file/shell/mysql
 cd $DEMO_MYSQL_DIR
 docker build -t wumugulu/mysql:0.2 .
 docker rm -f test-mysql
 docker run -d --name test-mysql -e MYSQL_ROOT_PASSWORD=123456 -p 3306:3306 -p 33060:33060 wumugulu/mysql:0.2
 
-# 构建,并运行自己的app容器
+# 3.构建,并运行自己的app容器
 echo
 echo ------------------2.3 buile apps image and run my apps container......
 echo
-# 3.准备jar包及启动脚本
+
+# 3.1 准备jar包及启动脚本
+DEMO_PROJECT_DIR=/home/apps/dev-test
 DEMO_APPS_DIR=$DEMO_PROJECT_DIR/file/shell/apps
 sudo rm -rf $DEMO_APPS_DIR/jar
 sudo mkdir -p $DEMO_APPS_DIR/jar
+
 cd $DEMO_APPS_DIR
 
 sudo cp -f $DEMO_PROJECT_DIR/TestGateway/target/TestGateway-1.0.jar ./jar
@@ -58,14 +61,13 @@ sudo cp -f $DEMO_PROJECT_DIR/TestService/TestUser/target/TestUser-1.0.jar ./jar
 sudo cp -f $DEMO_PROJECT_DIR/TestWeb/target/TestWeb-1.0.jar ./jar
 sudo cp -f $DEMO_PROJECT_DIR/file/shell/apps/startup.sh ./jar
 
-# 2.构建apps镜像
+# 3.2 构建apps镜像
 docker rmi -f wumugulu/myapps:0.3
 docker build -t wumugulu/myapps:0.3 .
 
-# 3.启动apps镜像
+# 3.3 启动apps容器
 docker rm -f test-apps
 docker run -d --name test-apps -p 8000:8000 --link test-nacos:nacos-host --link test-mysql:mysql-host wumugulu/myapps:0.3
-# docker run -it --name test-apps -p 8000:8000 --link test-nacos:nacos-host --link test-mysql:mysql-host wumugulu/myapps:0.3 /bin/bash
 
 docker ps
 # docker logs -f test-apps
